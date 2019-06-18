9d00e5e3fb5a9f8beceb77bab123cc270bbff7cf
Panni <simaianna3@gmail.com>
2019-06-18 13:49:43 +0200

    works

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -3,10 +3,13 @@ import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
 const SingleCharacterQuery = gql`
-  query($character: String!) {
-    characters(filter: { name: $character }) {
+  query($page: Int!, $character: String!) {
+    characters(page: $page, filter: { name: $character }) {
       info {
         count
+        next
+        prev
+        pages
       }
       results {
         name
@@ -18,6 +21,7 @@ const SingleCharacterQuery = gql`
 `;
 
 const SingleCharacter = () => {
+  const [page, setPage] = useState(1);
   const [character, setCharacter] = useState("Rick");
 
   return (
@@ -27,27 +31,63 @@ const SingleCharacter = () => {
         value={character}
         onChange={e => setCharacter(e.target.value)}
       />
-      <Query variables={{ character }} query={SingleCharacterQuery}>
+      <Query variables={{ page, character }} query={SingleCharacterQuery}>
         {({
           loading,
           error,
-          data: { characters: { info, results } = {} } = {}
+          data: {
+            characters: {
+              info: { next, prev, pages, count } = {},
+              results
+            } = {}
+          } = {}
         }) => {
           console.log(loading, error, results);
 
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error :(</p>;
+
+          next = next ? next : 1;
+          prev = prev ? prev : 1;
           return (
             <>
-              {results.map(({ name, id }) => (
-                <p key={id}>{name}</p>
-              ))}
+              {count > 0 && count}
+              {results ? (
+                results.map(({ name, id }) => <p key={id}>{name}</p>)
+              ) : (
+                <p>no results</p>
+              )}
+              <button type="button" onClick={() => setPage(prev)}>
+                Prev
+              </button>
+              <button type="button" onClick={() => setPage(next)}>
+                Next
+              </button>
+              <div>{paginationButton(pages, setPage, next - 1)}</div>
             </>
           );
         }}
       </Query>
     </div>
   );
+};
+
+const paginationButton = (pageCount, setPage, currentPage) => {
+  const pageButtons = [];
+  for (let i = 1; i <= pageCount; i++) {
+    pageButtons.push(
+      <button
+        className={currentPage === i ? "btn active" : "btn"}
+        key={i}
+        onClick={() => setPage(i)}
+      >
+        {i}
+      </button>
+    );
+  }
+
+  console.log(pageButtons);
+  return pageButtons;
 };
 
 export default SingleCharacter;
